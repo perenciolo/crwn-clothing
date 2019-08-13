b5eeb82b7e15dc0db2ba30b33b70e3e383c54b12
Gustavo Perenciolo <gustavoperenciolo@MacBook-Pro-de-Gustavo.local>
2019-08-12 21:15:24 -0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
-
 import Homepage from './pages/homepage.component';
-
+const Hat = () => <div>Hats route</div>;
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/hats" component={Hat} />
+      </Switch>
     </div>
   );
 }
